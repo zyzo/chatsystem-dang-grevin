96118cab4653bfad8dec5aaae65957969029fd27
danghaian168@gmail.com <danghaian168@gmail.com@8058bec7-3d9e-0887-4c99-2f0c36da2722>
2014-11-10 21:36:18 +0000

    working version of TCP-based CommunicationWindow (single-threaded)

--- trunk/TP-POO-An/src/test/TestTCPServer.java
+++ trunk/TP-POO-An/src/test/TestTCPServer.java
@@ -1,17 +1,23 @@
 package test;
 
+import gui.CommunicationWindow;
+
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.net.Socket;
 
 import net.CommunicaTCPServer;
 
-public class TestTCP {
+public class TestTCPServer {
      public static void main(String[] args) throws IOException {
     	 CommunicaTCPServer server = new CommunicaTCPServer(1234);
     	 Socket s = server.accept();
     	 BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-    	 System.out.println("Message received : " + in.readLine());
+    	 BufferedWriter out = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+    	 CommunicationWindow gui = new CommunicationWindow("lebotlan", out, in);
      }
 }

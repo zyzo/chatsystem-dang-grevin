2c679a4daf1babb39969ce2ed4ff31c1e156cdac
arthur.grevin@gmail.com <arthur.grevin@gmail.com@8058bec7-3d9e-0887-4c99-2f0c36da2722>
2014-11-27 22:50:16 +0000

    Début ChatNI-UDPSender et UDPReceiver, Principe envoi OK Receveur OK Identification des données reçues PAS OK

--- trunk/ChatSystem/src/main/java/boundaries/network/UDPSender.java
+++ trunk/ChatSystem/src/main/java/boundaries/network/UDPSender.java
@@ -1,5 +1,27 @@
 package boundaries.network;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class UDPSender {
 
+	private DatagramSocket socket;
+	private int port;
+	
+	public UDPSender(DatagramSocket socket, int port){
+		this.socket=socket;
+		this.port=port;
+	}
+	public void send(byte[] buffer,InetAddress address){
+		DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address,port);
+		try {
+			socket.send(packet);
+			System.out.println("Envoi de " +buffer);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }

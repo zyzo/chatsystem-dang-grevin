5a74f13852dabeae64a88da1959790edd35ede0a
danghaian168@gmail.com <danghaian168@gmail.com@8058bec7-3d9e-0887-4c99-2f0c36da2722>
2014-12-15 21:47:03 +0000

    add test suites

---
+++ ChatSystem/src/test/java/boundaries/swing/frame/UserListWindowTest.java
@@ -0,0 +1,21 @@
+package boundaries.swing.frame;
+
+import java.awt.EventQueue;
+
+import boundaries.swing.ChatGUI;
+
+public class UserListWindowTest {
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UserListWindow frame = new UserListWindow(ChatGUI.getInstance(), "hoho");
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}

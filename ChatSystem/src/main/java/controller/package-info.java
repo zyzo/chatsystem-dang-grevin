8ac551efcb2cae762d9c4e4e2ddd002a8b77a732
danghaian168@gmail.com <danghaian168@gmail.com@8058bec7-3d9e-0887-4c99-2f0c36da2722>
2014-12-17 17:35:44 +0000

    add package-info.java files (package level comments)

---
+++ ChatSystem/src/main/java/controller/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * The linker of different entities of the system (network interface,  <br>
+ * GUI, model) and the guide of information flows <br>
+ * 
+ * @author Arthur & Hai An
+ *
+ */
+package controller;
